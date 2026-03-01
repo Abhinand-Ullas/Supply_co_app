3449bab84ad34013263174e9f4186244a3a05107
Ikram-2004 <149678364+Ikram-2004@users.noreply.github.com>
2026-03-01 15:20:13 +0530

    back to normal

--- lib/intro_pages/splashscreen.dart
+++ lib/intro_pages/splashscreen.dart
@@ -19,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
     await Future.delayed(Duration(seconds: 3),
     () {
-          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> HomePage()));
+          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> AuthPage()));
     }
 
     );
