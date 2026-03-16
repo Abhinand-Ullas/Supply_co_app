9ca7c00cd9b9a05c4c141cf75a2673f7a9c5dc9f
SAGAR P <149305792+sagarp-c@users.noreply.github.com>
2026-03-17 00:24:03 +0530

    Delete django_database/databaseA directory

--- django_database/databaseA/inventory/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Store, Item, Stock
-
-admin.site.register(Store)
-admin.site.register(Item)
-admin.site.register(Stock)
--- django_database/databaseA/inventory/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import updated_stock
-
-urlpatterns = [
-    path('updated-stock/', updated_stock),
-]
